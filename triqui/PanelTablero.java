34cafbabec9df31338ac92ef0384d8f1e9a4219e
JhonTejada <105526926+JhonTejada@users.noreply.github.com>
2022-05-13 19:54:22 -0500

    Create PanelTablero.java

---
+++ triqui/PanelTablero.java
@@ -0,0 +1,15 @@
+
+package com.usc.ui;
+
+import javax.swing.*;
+
+public class PanelTablero extends JPanel  {
+    
+   JLabel etiquetaprueba;
+    
+    public  PanelTablero(){
+        
+        etiquetaprueba = new JLabel("aqui va el tablero ");
+        this.add(etiquetaprueba);
+    }
+}
